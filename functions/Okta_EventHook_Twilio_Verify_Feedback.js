0463eabc2416005d0527783b304f8628ccd5698a
mingchaoma <49459566+mingchaoma@users.noreply.github.com>
2024-09-04 17:20:46 +0100

    replace getUser API with listFactors API
    in Okta, a user profile might not have a phone number or the phone number might be different from enrolled factor (such as SMS or CALL factors) phone number; thus replace getUserAPI (which will retrieve phone number from user's profile) with  ListFactors API, which will list all enrolled factors of the user, and then grab the phone number based on the factor (sms or call) used during the MFA process

--- functions/Okta_EventHook_Twilio_Verify_Feedback.js
+++ functions/Okta_EventHook_Twilio_Verify_Feedback.js
@@ -19,17 +19,36 @@ try {
     console.log ("event data: ",event.data);
     let mfa_event= event.data?.events[0];
     console.log ("mfa_event: ", mfa_event);
-
+    let channel=null;
     //check payload of "user.authentication.auth_via_mfa" and "user.mfa.factor.activate" for SMS OTP and CALL OTP
-    if (mfa_event && mfa_event.outcome?.result === 'SUCCESS'  && (mfa_event.debugContext?.debugData?.factor === 'SMS_FACTOR' || mfa_event.debugContext?.debugData?.factor === 'CALL_FACTOR') || (mfa_event.outcome?.reason.includes("SMS_FACTOR") ||mfa_event.outcome?.reason.includes("CALL_FACTOR")))
+    if (mfa_event && mfa_event.outcome?.result === 'SUCCESS'  && (mfa_event.debugContext?.debugData?.factor === 'SMS_FACTOR' || mfa_event.outcome?.reason.includes("SMS_FACTOR"))) 
+    {channel="sms"} //SMS OTP is used
+    else if   (mfa_event && mfa_event.outcome?.result === 'SUCCESS'  && (mfa_event.debugContext?.debugData?.factor === 'CALL_FACTOR' ||mfa_event.outcome?.reason.includes("CALL_FACTOR"))) 
+    {channel="call"} //call OTP is used
+
+    if (channel!==null)
     {
-    //call Okta user API with user id to get the phone number, which will be used when calling Twilio Verify feedback API
     const userid=mfa_event.actor?.id;//grab user id
     const okta=require('@okta/okta-sdk-nodejs');
     const OktaClient= new okta.Client({ orgUrl: oktaBaseUrl, token: api_token });
-    let user = await OktaClient.userApi.getUser({ userId: userid }); //call Okta user API
-    const phone_number=user.profile.mobilePhone.replace(/\s+/g, ''); //grab phone numberand remove space in between
-    console.log ("user phone number: ", phone_number);
+    let phone_number=null;
+    //get the list of enrolled factors (such as sms, call etc)
+    const factors= await OktaClient.userFactorApi.listFactors({ userId: userid });
+
+    for await (const factor of factors){
+
+      //in Okta, user can enroll different phone numbers for SMS factor and call factor respectively, thus have to grab the phone number based on the actual factor used in MFA
+      if (factor.factorType===channel) {
+         phone_number=factor.profile.phoneNumber;
+          console.log ("MFA factor is:", channel);
+          console.log ("MFA phone number is:", phone_number);
+      }
+    }
+    if (phone_number===null) {
+            console.log ("can't retrieve phone number, possible not SMS OTP or Voice OTP MFA factor")
+        return callback (null, "can't retrieve phone number, possible not SMS OTP or Voice OTP MFA factor")
+    }
+    
    //call Verify feedback API using phone number
     let client = context.getTwilioClient();
     let verification=await client.verify.v2.services(context.VERIFY_SID)
